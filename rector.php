a09f3859e24c0d46c017d3e4c686215c11518d0e
ahwet-sen <ahwt93sn@gmail.com>
2025-04-18 12:52:08 +0300

    Add Code Quality Modern Php - Laravel Pint & Rector

---
+++ rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+
+return RectorConfig::configure()
+    ->withSkip([
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ])
+    ->withPaths([
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/lang',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/src',
+        __DIR__.'/stubs',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withPhpSets();
